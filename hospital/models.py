aa4752bde253ecbc48e17be681eb25e524a5c6ea
Kanzariya Kanji SureshBhai <132264003+Kanji1812@users.noreply.github.com>
2025-07-17 15:43:14 +0530

    chnage models

--- hospital/models.py
+++ hospital/models.py
@@ -34,19 +34,25 @@ class User(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     objects = CustomUserManager() 
 
-class Slot(models.Model):
+class Slot(BaseModel):
     doctor = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'doctor'})
     date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     is_booked = models.BooleanField(default=False)
 
-class Appointment(models.Model):
+    @staticmethod
+    def available_slots(doctor):
+        return Slot.objects.filter(doctor=doctor, is_booked=False)
+    
+class Appointment(BaseModel):
     STATUS_CHOICES = (("pending", "Pending"), ("completed", "Completed"), ("cancelled", "Cancelled"))
 
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="appointments", limit_choices_to={'role': 'patient'})
     slot = models.OneToOneField(Slot, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
+    def __str__(self):
+        return f"Appointment for {self.patient.email} with {self.doctor.email} on {self.slot.date} from {self.slot.start_time} to {self.slot.end_time}"
 
     @property
     def doctor(self):
